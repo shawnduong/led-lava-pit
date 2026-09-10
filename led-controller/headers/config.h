5e7de0cde4e3809aedd9216a798f8fb78ac6903d
Shawn Duong <me@shawnd.xyz>
2026-09-09 18:07:02 -0700

    Added docstrings to .h files.

--- led-controller/headers/config.h
+++ led-controller/headers/config.h
@@ -1,8 +1,12 @@
 #pragma once
 
+/* Change these based on your wiring, if needed. */
 #define KY_CLK 4
 #define KY_DT  3
 #define KY_SW  2
 
+/* Initialize the configuration module. */
 void init_config();
+
+/* See if there's a knob press and enter config mode if so. */
 void poll_config_mode();
--- led-controller/headers/lcd.h
+++ led-controller/headers/lcd.h
@@ -1,9 +1,18 @@
 #pragma once
 
+/* Change these based on your LCD module, if needed. */
 #define LCD_COLUMNS  16
 #define LCD_ROWS      2
 
+/* Initialize the LCD module. */
 void init_lcd(uint8_t address);
+
+/* Clear the LCD screen. */
 void clear_lcd();
+
+/* Overwrite the entire LCD screen with given text contents. Every input string
+   is another line on the LCD screen. */
 void write_lcd(char *text, ...);
+
+/* Write a string to a specific row on the LCD. */
 void write_lcd_row(uint8_t row, char *text);
